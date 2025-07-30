5a7db43730cbaf25d7328e7d7ae6838c46155cba
ShreyaSingh-14 <86734942+Ash1412005Fame@users.noreply.github.com>
2025-07-30 23:40:53 +0530

    changes made in app/layout and app/index

---
+++ trip-planner/app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+export default function Index() {
+  return (
+    <View style={{ 
+        flex: 1,
+         justifyContent: 'center',
+          alignItems: 'center'
+           }}>
+      <Text>Welcome to the Trip Planner App!</Text>
+    </View>
+  );
+}
