d0105f9ff9ae3d97d34e1e28e027574d91ddbbca
Shivani Yadav <ishivaniyadav7@gmail.com>
2025-08-11 23:18:25 +0530

    Shivani: completed the homepage

--- trip-planner/app/(tabs)/_layout.jsx
+++ trip-planner/app/(tabs)/_layout.jsx
@@ -1,13 +1,34 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import { Colors } from './../../constants/Colors';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function TabLayout() {
   return (
-   <Tabs screenOptions={{headerShown: false}}>
-   <Tabs.Screen name="mytrip" />
-   <Tabs.Screen name="discover" />
-   <Tabs.Screen name="profile" />
+   <Tabs screenOptions={{
+    headerShown: false,
+    tabBarActiveTintColor: Colors.PRIMARY,
+   }}>
+     <Tabs.Screen name="mytrip" 
+     options ={{
+      tabBarLabel: 'My Trip',
+      tabBarIcon: ({ color }) =><Ionicons name = "location-sharp" 
+      size={24} color={color} /> 
+     }}/>
+     <Tabs.Screen name="discover"
+      options ={{
+      tabBarLabel: 'Discover',
+      tabBarIcon: ({ color }) =><Ionicons name="globe-sharp" 
+      size={24} color={color} /> 
+     }} />
+    <Tabs.Screen name="profile" 
+     options ={{
+      tabBarLabel: 'Profile',
+      tabBarIcon: ({ color }) =><Ionicons name="people-circle" 
+      size={24} color={color} /> 
+     }} />
    </Tabs>    
   )
 }
---
+++ trip-planner/components/MyTrips/StartNewTripCard.jsx
@@ -0,0 +1,46 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Colors } from './../../constants/Colors';
+
+export default function StartNewTripCard() {
+  return (
+    <View
+    style={{
+      padding: 20,  
+      marginTop: 50,
+      display: 'flex',
+      alignItems: 'center',  
+      gap: 25
+    }}>
+      <Ionicons name = "location-sharp" size={30} color={Colors.PRIMARY} />
+      <Text
+      style={{
+        fontSize: 20,
+        fontFamily: 'outfit-medium',
+      }}> No Trips Planned Yet...</Text>  
+        
+      <Text
+        style={{
+            fontSize: 20,
+            fontFamily: 'outfit',
+            textAlign: 'center',
+            color: Colors.GRAY,
+        }}> Looks like it's time to plan a new travel experience! Get Started below</Text> 
+    
+        <TouchableOpacity 
+        style={{
+            padding: 15,    
+            backgroundColor: Colors.PRIMARY,
+            borderRadius: 15,
+            paddingHorizontal: 30
+        }}>
+            <Text style={{
+                color: Colors.WHITE,
+                fontFamily: 'outfit-medium',
+                fontSize: 17,
+            }}>Start a new trip</Text>
+        </TouchableOpacity>
+    </View>
+  )
+}
