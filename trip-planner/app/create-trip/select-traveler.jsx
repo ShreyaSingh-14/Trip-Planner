1794640359b97d19181d6ebb040b17b64b3020f8
ShreyaSingh-14 <86734942+Ash1412005Fame@users.noreply.github.com>
2025-08-12 23:40:47 +0530

    shreya: updating select date

--- trip-planner/app/create-trip/select-traveler.jsx
+++ trip-planner/app/create-trip/select-traveler.jsx
@@ -1,31 +1,61 @@
-import { View, Text } from 'react-native'
-import React, { useEffect } from 'react'
-import { useNavigation } from 'expo-router';
-import { useRouter } from 'expo-router';
+import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useEffect } from 'react';
+import { Link, useNavigation } from 'expo-router';
 
 export default function SelectTraveler() {
-    const navigation = useNavigation();
-    useEffect(() => {
-        navigation.setOptions({ 
-            headerShown: true,
-            headerTransparent: true,
-            headerTitle: '',
-        });}, []);
+  const navigation = useNavigation();
+
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: true,
+      headerTransparent: true,
+      headerTitle: '',
+    });
+  }, []);
+
   return (
-     <View style={{ 
-        paddingTop: 25, 
-     paddingTop: 75, 
-     backgroundColor: '#fff',
-     height: '100%',
-     }}>
-        <Text style={{
-            fontFamily: 'outfit-bold',
-            fontSize: 35,
-            paddingLeft: 25,
-            color: '#000',
-            marginTop: 20,
+    <View
+      style={{
+        paddingTop: 75,
+        backgroundColor: '#fff',
+        height: '100%',
+      }}
+    >
+      <Text
+        style={{
+          fontFamily: 'outfit-bold',
+          fontSize: 35,
+          paddingLeft: 25,
+          color: '#000',
+          marginTop: 20,
         }}
-        >Who is Travelling?</Text>
-        </View>
-)
+      >
+        Who is Travelling?
+      </Text>
+
+      <TouchableOpacity
+        style={{
+          marginTop: 30,
+          marginHorizontal: 100,
+          backgroundColor: '#000',
+          paddingVertical: 12,
+          borderRadius: 8,
+        }}
+      >
+         <Link href={'/create-trip/select-dates'}>
+        <Text
+          style={{
+            color: '#fff',
+            fontSize: 16,
+            fontFamily: 'outfit',
+            textAlign: 'center',
+          }}
+        >
+          Go to Next Page
+        </Text>
+         </Link>
+      </TouchableOpacity>
+    </View>
+  );
 }
